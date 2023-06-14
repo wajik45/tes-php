ab340866b0787896ee32ecdba66f1af33de93aed
wajik45 <haikal2589780@gmail.com>
2023-06-14 17:22:09 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+function sayHello($nama, $alamat) {
+    return "Hallo nama saya $nama saya tinggal di $alamat";
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP DIKIT</title>
+</head>
+<body>
+    <h1><?php echo sayHello('Udin', 'Lengkong'); ?></h1>
+</body>
+</html>
